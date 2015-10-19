bf9ce6bbdfb07aab54863ad63225389895d2ce66
jamesknelson <james@jamesknelson.com>
2015-10-19 18:21:36 +0900

    namespace types with 'app'

--- src/utils/defineActionTypes.js
+++ src/utils/defineActionTypes.js
@@ -23,7 +23,7 @@ export default function defineActionTypes(obj) {
         "Types must start with a capital letter, and be composed entirely of capital letters, numbers, and the underscore character."
       )
 
-      namespaceTypes[type] = `${namespace}-${type}`
+      namespaceTypes[type] = `@@app/${namespace}/${type}`
     }
 
     result[namespace] = namespaceTypes
